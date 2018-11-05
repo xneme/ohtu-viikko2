9c46f3cb9a0d96c3a53d9820e07192d6f235b0d2
Jere Helén <jere.helen@helsinki.fi>
2018-11-05 18:01:13 +0200

    Dependancy injection with annotation

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,9 +1,14 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Pankki implements PankkiInterface {
 
     private KirjanpitoInterface kirjanpito;
 
+    @Autowired
     public Pankki(KirjanpitoInterface kirjanpito) {
         this.kirjanpito = kirjanpito;
     }
