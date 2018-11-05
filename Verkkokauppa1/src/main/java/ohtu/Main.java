acfafacdfb290205fd9d2c0be10313c8234d42a3
Jere Helén <jere.helen@helsinki.fi>
2018-11-05 16:13:41 +0200

    Dependency injection with Spring

--- Verkkokauppa1/src/main/java/ohtu/Main.java
+++ Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -5,15 +5,19 @@ import ohtu.verkkokauppa.Kirjanpito;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        Kirjanpito kirjanpito = new Kirjanpito();
-        Varasto varasto = new Varasto(kirjanpito);
-        Pankki pankki = new Pankki(kirjanpito);
-        Viitegeneraattori viitegen = new Viitegeneraattori();
-        Kauppa kauppa = new Kauppa(varasto, pankki, viitegen);
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+
+        Kirjanpito kirjanpito = ctx.getBean(Kirjanpito.class);
+        Varasto varasto = ctx.getBean(Varasto.class);
+        Pankki pankki = ctx.getBean(Pankki.class);
+        Viitegeneraattori viitegen = ctx.getBean(Viitegeneraattori.class);
+        Kauppa kauppa = ctx.getBean(Kauppa.class);
         
         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
